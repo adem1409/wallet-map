ade333d58fcb8dae94a00f0a3b2cf0e5c207b0bc
Sammoudi <adem.sammoudi1409@gmail.com>
2024-12-16 14:53:58 +0100

    add error message in login

--- src/components/auth/login/LoginForm.jsx
+++ src/components/auth/login/LoginForm.jsx
@@ -33,9 +33,12 @@ export default function LoginForm() {
     } catch (err) {
       const msg = err?.response?.data?.message;
       if (msg === "EmailAlreadyExists") {
-        setError("apiError", { type: "manual", message: "Email already exists" });
+        setError("apiError", {
+          type: "manual",
+          message: "Email already exists",
+        });
       } else {
-        setError("apiError", { type: "manual", message: msg });
+        setError("apiError", { type: "manual", message: err.response.data });
       }
     }
   }
@@ -52,7 +55,9 @@ export default function LoginForm() {
           placeholder="Enter Email"
           className="block w-full mt-2 px-3 py-2 rounded-lg border border-slate-200 outline-none focus:ring-1 ring-slate-400 duration-200"
         />
-        {errors.email?.message && <p className="mt-1 text-red-500">{errors.email?.message}</p>}
+        {errors.email?.message && (
+          <p className="mt-1 text-red-500">{errors.email?.message}</p>
+        )}
       </div>
       <div className="mt-2">
         <label className="font-medium text-gray" htmlFor="password">
@@ -73,10 +78,16 @@ export default function LoginForm() {
             }}
             className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
           >
-            {showPassword ? <EyeSlashIcon className="size-5" /> : <EyeIcon className="size-5" />}
+            {showPassword ? (
+              <EyeSlashIcon className="size-5" />
+            ) : (
+              <EyeIcon className="size-5" />
+            )}
           </button>
         </div>
-        {errors.password?.message && <p className="mt-1 text-red-500">{errors.password?.message}</p>}
+        {errors.password?.message && (
+          <p className="mt-1 text-red-500">{errors.password?.message}</p>
+        )}
       </div>
       <button
         disabled={isSubmitting}
@@ -92,13 +103,22 @@ export default function LoginForm() {
           </>
         )}
       </button>
-      {errors.apiError?.message && <p className="mt-1 text-red-500">{errors.apiError?.message}</p>}
+      {errors.apiError?.message && (
+        <p className="mt-2 text-red-500 text-center">
+          {errors.apiError?.message}
+        </p>
+      )}
     </form>
   );
 }
 
 const LoginIcon = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 512 512" {...props}>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+    viewBox="0 0 512 512"
+    {...props}
+  >
     <path d="m217.9 105.9 122.8 122.8c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L217.9 406.1c-6.4 6.4-15 9.9-24 9.9-18.7 0-33.9-15.2-33.9-33.9V320H32c-17.7 0-32-14.3-32-32v-64c0-17.7 14.3-32 32-32h128v-62.1c0-18.7 15.2-33.9 33.9-33.9 9 0 17.6 3.6 24 9.9M352 416h64c17.7 0 32-14.3 32-32V128c0-17.7-14.3-32-32-32h-64c-17.7 0-32-14.3-32-32s14.3-32 32-32h64c53 0 96 43 96 96v256c0 53-43 96-96 96h-64c-17.7 0-32-14.3-32-32s14.3-32 32-32"></path>
   </svg>
 );
