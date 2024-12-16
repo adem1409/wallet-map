5db7482c206e0f5ea0bb58ab236ba2edcc5eccbe
Adem Sammoudi <adem.sammoudi1409@gmail.com>
2024-12-16 22:16:40 +0100

    add Transactions sidebar section

---
+++ src/components/app/transactions/Transactions.jsx
@@ -0,0 +1,9 @@
+function Transactions() {
+  return (
+    <div className="">
+      <h3 className="text-2xl font-semibold text-slate-700">Transactions</h3>
+    </div>
+  );
+}
+
+export default Transactions;
