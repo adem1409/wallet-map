54a17d4fee32ac5a778957ef12a0364b13dd4aff
Atri Omar <atri.omar.2003@gmail.com>
2024-12-19 18:02:20 +0100

    Add create transaction

--- src/components/app/debt-manager/[id]/AddTransaction.jsx
+++ src/components/app/debt-manager/[id]/AddTransaction.jsx
@@ -1,19 +1,26 @@
 "use client";
 
+import CustomToast from "@/components/CustomToast";
 import { RingLoader } from "@/components/Loaders";
 import { DateTimePicker } from "@/components/app/debt-manager/[id]/DateTimePicker";
 import { useAuthContext } from "@/contexts/AuthProvider";
 import { ArrowsRightLeftIcon, ChevronRightIcon, PlusIcon } from "@heroicons/react/20/solid";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "@/config/axios";
 import Image from "next/image";
 import { useMemo } from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import { z } from "zod";
 
 const schema = z.object({
-  amount: z.string().regex(/^\d+(\.\d{0,2})?$/, "Amount must be a valid number with up to 2 decimal places."),
+  amount: z
+    .string()
+    .regex(/^\d+(\.\d{0,2})?$/, "Amount must be a valid number with up to 2 decimal places.")
+    .transform((val) => parseFloat(val)),
   label: z.string().min(1, "Label is required"),
   date: z.date(),
+  sideAIsLender: z.boolean(),
 });
 
 export default function AddTransaction({ transactions, contract, userSide, otherUserSide }) {
@@ -22,10 +29,11 @@ export default function AddTransaction({ transactions, contract, userSide, other
     handleSubmit,
     watch,
     setValue,
+    reset,
     formState: { errors, isSubmitting },
   } = useForm({
     defaultValues: {
-      userIsLender: true,
+      sideAIsLender: true,
       amount: "",
       label: "Transaction #1000",
       date: new Date(),
@@ -37,10 +45,7 @@ export default function AddTransaction({ transactions, contract, userSide, other
   const otherUser = useMemo(() => contract[otherUserSide], [otherUserSide]);
 
   const formValues = watch();
-  const { userIsLender, date } = formValues;
-
-  console.log("-------------------- formValues --------------------");
-  console.log(formValues);
+  const { sideAIsLender, date } = formValues;
 
   const onInput = (e) => {
     // Only allow digits and a single optional decimal point with up to 2 decimal places
@@ -49,16 +54,33 @@ export default function AddTransaction({ transactions, contract, userSide, other
     e.target.value = match ? match[0] : ""; // Update value to valid portion
   };
 
-  const onSubmit = (data) => {
-    console.log("Submitted data:", data);
-  };
+  async function onSubmit(_data) {
+    const data = { ..._data };
+
+    if (!sideAIsLender) {
+      data.amount = -data.amount;
+    }
+
+    data.contract = contract.id;
+
+    try {
+      await axios.post("/api/transactions", data);
+      toast.custom((t) => <CustomToast t={t} message={"Transaction added successfully"} />);
+      reset();
+      // fetchContracts();
+    } catch (err) {
+      console.log(err);
+      toast.error("An error occurred while submitting the form");
+    }
+    // fetchAppointments();
+  }
 
   return (
     <div className="mt-4 bg-white border border-slate-200 rounded-lg py-2 px-3">
       <h2 className="text-lg font-semibold">New Transaction</h2>
       <form onSubmit={handleSubmit(onSubmit)} className="w-fit">
         <label className="block w-fit mt-2 p-2 rounded-lg hover:bg-slate-200/75 has-[:focus-visible]:bg-slate-200/75 duration-200 cursor-pointer group">
-          <input type="checkbox" className="sr-only" {...register("userIsLender")} />
+          <input type="checkbox" className="sr-only" {...register("sideAIsLender")} />
           <div className="w-fit py-0.5 px-1.5 mb-[-10px] mx-auto rounded-lg bg-slate-200/85 group-hover:bg-slate-300 group-hover:scale-110 group-has-[:focus-visible]:bg-slate-300 group-has-[:focus-visible]:scale-110 duration-200">
             <ArrowsRightLeftIcon className="size-[18px] text-slate-700 group-hover:text-black group-has-[:focus-visible]:text-black duration-200" />
           </div>
@@ -70,16 +92,16 @@ export default function AddTransaction({ transactions, contract, userSide, other
               <p className="font-medium text-sm text-slate-700">You</p>
             </div>
             <div>
-              <div className={`flex items-center ${userIsLender ? "" : "[transform:rotateY(180deg)]"} duration-500`}>
-                <div className={`w-[150px] h-[2.5px] ml-2 rounded ${userIsLender ? "bg-green-700" : "bg-red-500"}`}></div>
-                <ChevronRightIcon className={`size-7 ml-[-17px] ${userIsLender ? "text-green-700" : "text-red-500"}`} />
+              <div className={`flex items-center ${sideAIsLender ? "" : "[transform:rotateY(180deg)]"} duration-500`}>
+                <div className={`w-[150px] h-[2.5px] ml-2 rounded ${sideAIsLender ? "bg-green-700" : "bg-red-500"}`}></div>
+                <ChevronRightIcon className={`size-7 ml-[-17px] ${sideAIsLender ? "text-green-700" : "text-red-500"}`} />
               </div>
               {/* <p className="-mt-1 font-medium text-[15px] text-green-700 text-center">Lend</p> */}
 
               {/*  ---------------------------- FLIP CARD ---------------------------- */}
               {/*  ---------------------------- FLIP CARD ---------------------------- */}
-              <div className={`[perspective:1000px] w-40 h-6 mx-auto ${userIsLender ? "text-green-700" : "text-red-500"}`}>
-                <div className={`h-full w-full preserve-3d ${userIsLender ? "" : "rotate-y-180"} relative duration-500 font-medium`}>
+              <div className={`[perspective:1000px] w-40 h-6 mx-auto ${sideAIsLender ? "text-green-700" : "text-red-500"}`}>
+                <div className={`h-full w-full preserve-3d ${sideAIsLender ? "" : "rotate-y-180"} relative duration-500 font-medium`}>
                   <div className="backface-hidden absolute top-0 bottom-0 right-0 left-0 flex items-center justify-center">
                     <h2>Lend</h2>
                   </div>
